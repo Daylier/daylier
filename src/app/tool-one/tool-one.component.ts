da6ef9972a6d718957d4d332c21016c7b65dab4e
Jace Fugate <jace.fugate@tcnbroadcasting.com>
2022-11-29 22:08:48 -0700

    prove that we can do a simple convert from png to jpeg

--- src/app/tool-one/tool-one.component.ts
+++ src/app/tool-one/tool-one.component.ts
@@ -8,10 +8,14 @@ import Jimp from 'jimp';
 })
 export class ToolOneComponent implements OnInit {
   constructor() {
+    // lets test if this works
     Jimp.read(
       'https://cdn.pixabay.com/photo/2014/06/03/19/38/road-sign-361514_1280.png'
     ).then((image: any) => {
       console.log(image);
+      image.getBase64(Jimp.MIME_JPEG, (err: any, src: any) => {
+        console.log(src);
+      });
     });
   }
 
