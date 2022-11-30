9a035b20aec5edaa690dbc0a8bdf974322f5f4cd
Jace Fugate <jace.fugate@tcnbroadcasting.com>
2022-11-29 21:57:50 -0700

    get jimp loading by adding a bunch of polyfils for nodejs stuff

--- src/app/tool-one/tool-one.component.ts
+++ src/app/tool-one/tool-one.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import Jimp from 'jimp';
 
 @Component({
   selector: 'app-tool-one',
   templateUrl: './tool-one.component.html',
-  styleUrls: ['./tool-one.component.scss']
+  styleUrls: ['./tool-one.component.scss'],
 })
 export class ToolOneComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    Jimp.read(
+      'https://cdn.pixabay.com/photo/2014/06/03/19/38/road-sign-361514_1280.png'
+    ).then((image: any) => {
+      console.log(image);
+    });
   }
 
+  ngOnInit(): void {}
 }
