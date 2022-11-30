e10e53a30d553b8fb999b63108e5b573d994350e
Jace Fugate <jace.fugate@tcnbroadcasting.com>
2022-11-29 22:00:20 -0700

    remove unneeded webpack "resolve" settings

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -3,9 +3,4 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = {
   // Other rules...
   plugins: [new NodePolyfillPlugin()],
-  resolve: {
-    fallback: {
-      fs: false,
-    },
-  },
 };
